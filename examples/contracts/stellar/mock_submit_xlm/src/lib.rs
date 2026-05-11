5cbcf177ac3c846c424b88c88a50549b4c3cc92a
Merkle Maren <266985194+merkle-maren@users.noreply.github.com>
2026-05-11 10:39:56 +0000

    Add new xlm mock submit

---
+++ examples/contracts/stellar/mock_submit_xlm/src/lib.rs
@@ -0,0 +1,8 @@
+#![no_std]
+
+mod contract;
+pub mod storage;
+
+pub use contract::StellarHandler;
+pub use contract::StellarHandlerClient;
+pub use warpdrive_shared::interfaces::handler::{Ed25519SignatureData, HandlerError};
